a90337c44ea0b618d439111b48d31209ac906801
CarlUdo <carl@wbsweden.com>
2024-11-13 09:20:38 +0100

    Add message for no ice creams found

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <h1>Ice creams</h1>;
+  return (
+    <>
+      <h1>Ice creams</h1>;
+      <p>No Ice-cream found. Please add an ice cream</p>
+    </>  
+  );
+  
 }
