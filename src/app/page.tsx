b0e28b91820417caef0d5debc90e48af0d915cb2
CarlUdo <carl@wbsweden.com>
2024-11-13 09:32:14 +0100

    Add mock service method for getting all ice creams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
-export default function Home() {
+function creatIceCreamService() {
+  return {
+    async getAll() {
+      return [];
+    }
+  };
+};
+
+const iceCreamService = creatIceCreamService();
+
+export default async function Home() {
+  const iceCreams = await iceCreamService.getAll();
+
   return (
     <>
-      <h1>Ice creams</h1>;
-      <p>No Ice-cream found. Please add an ice cream</p>
+      <h1>Ice creams</h1>
+      {
+        !iceCreams.length && (
+          <p>No Ice-cream found. Please add an ice cream</p>
+        )
+      }      
     </>  
   );
   
